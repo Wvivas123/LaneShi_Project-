3615093cffd97506ac180e9b45f16f6ce2594d19
William Vivas <williamvivasdev@gmail.com>
2020-07-19 18:30:46 -0400

    got videos in line

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function ($) {
   $('html, body').animate({ scrollTop: $('.animation1').offset().top }, 1000);
+
+  $('.fire').click(function () {
+    $('.animation1').fadeOut('slow', function () {
+      // Animation complete.
+    });
+  });
 });
