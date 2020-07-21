be226f79664f298226698a498c322d2e4a9adc7d
William Vivas <williamvivasdev@gmail.com>
2020-07-21 11:34:47 -0400

    continued with the logic of the game

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,53 @@ jQuery(document).ready(function ($) {
   let div1 = 1;
   let div2 = 3;
   let div3 = 5;
+  let click1 = true;
+  let click2 = true;
+  let click3 = true;
 
   $('html, body').animate({ scrollTop: $('.animation1').offset().top }, 1000);
-
-  //$('.topdiv').click(function () {
-  //  total++;
-  // console.log(total);
-  //});
+  // On click  increment to total
+  $('.topdiv').click(function () {
+    if (click1 === true) {
+      total = div1 + total;
+      console.log(total);
+      click1 = false;
+    } else {
+      console.log('it works');
+    }
+  });
+  $('.leftvid').click(function () {
+    if (click2 === true) {
+      total = div2 + total;
+      console.log(total);
+      click2 = false;
+    } else {
+      console.log('it works');
+    }
+  });
+  $('.rightvid').click(function () {
+    if (click3 === true) {
+      total = div3 + total;
+      console.log(total);
+      click3 = false;
+    } else {
+      console.log('it works');
+    }
+  });
 
   //On Click Dessolve the Body and display the Div
   $('.fire').click(function () {
     $('.animation1').fadeOut('slow', function () {
       // Animation complete.
-      $('.front').append(
-        '<audio src="assets/song1.mp3" controls autoplay></audio>'
-      );
+      if (total === 0) {
+        $('.front').append(
+          '<audio src="assets/track0.mp3" controls autoplay></audio>'
+        );
+      } else {
+        $('.front').append(
+          '<audio src="assets/song1.mp3" controls autoplay></audio>'
+        );
+      }
     });
   });
 });
