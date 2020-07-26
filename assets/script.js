016af33d7f044b7144600c591901d8210ed96c38
William Vivas <williamvivasdev@gmail.com>
2020-07-25 21:54:42 -0400

    got the new logic

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function ($) {
-  let total = 0;
-  let div1 = 1;
-  let div2 = 3;
-  let div3 = 5;
+  let total = [];
+
   let click1 = true;
   let click2 = true;
   let click3 = true;
@@ -14,17 +12,26 @@ jQuery(document).ready(function ($) {
     if (click1 === true && counter == 0) {
       $('.int').prepend('<span>1.</span>');
       counter++;
+      total.push(1);
       click1 = false;
+
+      console.log(total);
     }
     if (click1 === true && counter == 1) {
       $('.int').prepend('<span>2.</span>');
       counter++;
+      total.push(1);
       click1 = false;
+
+      console.log(total);
     }
     if (click1 === true && counter == 2) {
       $('.int').prepend('<span>3.</span>');
       counter++;
+      total.push(1);
       click1 = false;
+
+      console.log(total);
     } else {
       console.log('it works');
     }
@@ -34,17 +41,26 @@ jQuery(document).ready(function ($) {
     if (click2 === true && counter == 0) {
       $('.experience').prepend('<span>1.</span>');
       counter++;
+      total.push(2);
       click2 = false;
+
+      console.log(total);
     }
     if (click2 === true && counter == 1) {
       $('.experience').prepend('<span>2.</span>');
       counter++;
+      total.push(2);
       click2 = false;
+
+      console.log(total);
     }
     if (click2 === true && counter == 2) {
       $('.experience').prepend('<span>3.</span>');
       counter++;
+      total.push(2);
       click2 = false;
+
+      console.log(total);
     } else {
       console.log('it works');
     }
@@ -54,17 +70,26 @@ jQuery(document).ready(function ($) {
     if (click3 === true && counter == 0) {
       $('.hon').prepend('<span>1.</span>');
       counter++;
+      total.push(3);
       click3 = false;
+
+      console.log(total);
     }
     if (click3 === true && counter == 1) {
       $('.hon').prepend('<span>2.</span>');
       counter++;
+      total.push(3);
       click3 = false;
+
+      console.log(total);
     }
     if (click3 === true && counter == 2) {
       $('.hon').prepend('<span>3.</span>');
       counter++;
+      total.push(3);
       click3 = false;
+
+      console.log(total);
     } else {
       console.log('it works');
     }
@@ -74,39 +99,50 @@ jQuery(document).ready(function ($) {
   $('.lastflower').click(function () {
     $('.animation1').fadeOut('slow', function () {
       // Animation complete.
-      if (total === 0) {
+      if (
+        total.indexOf(1) == 0 &&
+        total.indexOf(2) == 1 &&
+        total.indexOf(3) == 2
+      ) {
         $('.front').append(
           '<audio src="assets/song0.mp3" controls autoplay></audio>'
         );
       }
-      if (total === 1) {
+      if (
+        total.indexOf(1) == 1 &&
+        total.indexOf(2) == 2 &&
+        total.indexOf(3) == 0
+      ) {
         $('.front').append(
           '<audio src="assets/song1.mp3" controls autoplay></audio>'
         );
       }
-      if (total === 3) {
+      if (
+        total.indexOf(1) == 2 &&
+        total.indexOf(2) == 0 &&
+        total.indexOf(3) == 1
+      ) {
         $('.front').append(
           '<audio src="assets/song3.mp3" controls autoplay></audio>'
         );
       }
-      if (total === 5) {
-        $('.front').append(
-          '<audio src="assets/song5.mp3" controls autoplay></audio>'
-        );
-      }
-      if (total === 4) {
+      if (
+        total.indexOf(1) == 2 &&
+        total.indexOf(2) == 1 &&
+        total.indexOf(3) == 0
+      ) {
         $('.front').append(
           '<audio src="assets/song4.mp3" controls autoplay></audio>'
         );
       }
-      if (total === 8) {
+
+      if (
+        total.indexOf(1) == 1 &&
+        total.indexOf(2) == 0 &&
+        total.indexOf(3) == 2
+      ) {
         $('.front').append(
-          '<audio src="assets/song8.mp3" controls autoplay></audio>'
-        );
-      }
-      if (total === 9) {
-        $('.front').append(
-          '<audio src="assets/song9.mp3" controls autoplay></audio>'
+          '<audio src="assets/song5.mp3" controls autoplay></audio>'
         );
       }
     });
